3a20b6900c583590704e996dd9610f54c73fe0c8
Silvia Witzig <silvia.witzig@unibas.ch>
2017-06-29 10:54:27 +0200

    rero Backlinks auf neue Oberfläche

--- module/Swissbib/src/Swissbib/XSLT/MARCFormatter.php
+++ module/Swissbib/src/Swissbib/XSLT/MARCFormatter.php
@@ -75,7 +75,7 @@ class MARCFormatter implements ServiceManagerAwareInterface
         "LIBIB" => "http://aleph.lbfl.li/F/?local_base=LLB01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
         "NEBIS" => "http://opac.nebis.ch/F/?local_base=EBI01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
         "OCoLC" => "http://www.worldcat.org/oclc/%s",
-        "RERO" => "http://opac.rero.ch/gateway?beginsrch=1&lng=de&inst=consortium&search=KEYWORD&function=CARDSCR&t1=%s&u1=12",
+        "RERO" => "http://data.test.rero.ch/01-%s/html",
         "RETROS" => "http://www.e-periodica.ch/oai/dataprovider?verb=GetRecord&metadataPrefix=oai_dc&identifier=%s",
         "SBT" => "http://aleph.sbt.ti.ch/F?local_base=SBT01&con_lng=ITA&func=find-b&find_code=SYS&request=%s",
         "SERVAL" => "http://serval.unil.ch/oaiprovider?verb=GetRecord&metadataPrefix=mods&identifier=oai:serval.unil.ch:%s",
