2ff92cb8db744562f2023d2f8beb0e3f2dc3b184
Silvia Witzig <silvia.witzig@unibas.ch>
2017-09-20 14:04:09 +0200

    Integration edoc: Anpassungen VuFind

--- module/Swissbib/src/Swissbib/XSLT/MARCFormatter.php
+++ module/Swissbib/src/Swissbib/XSLT/MARCFormatter.php
@@ -66,6 +66,7 @@ class MARCFormatter implements ServiceManagerAwareInterface
         "CHARCH" => "http://www.helveticarchives.ch/detail.aspx?ID=%s",
         "DDB" => "http://d-nb.info/%s",
         "ECOD" => "http://www.e-codices.unifr.ch/oai/oai.php?verb=GetRecord&metadataPrefix=oai_dc&identifier=oai:e-codices.unifr.ch:http://www.e-codices.unifr.ch/en/list/one/%s",
+        "EDOC" => "http://edoc.unibas.ch/cgi/oai2?verb=GetRecord&identifier=%s&metadataPrefix=oai_dc",
         "HAN" => "http://aleph.unibas.ch/F/?local_base=DSV05&con_lng=GER&func=find-b&find_code=SYS&request=%s",
         "HEMU" => "http://opacbiblio.hemu-cl.ch/cgi-bin/koha/opac-detail.pl?biblionumber=%s",
         "IDSBB" => "http://aleph.unibas.ch/F/?local_base=DSV01&con_lng=GER&func=find-b&find_code=SYS&request=%s",
