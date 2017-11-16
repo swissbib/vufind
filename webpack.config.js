9ab0ca08f82ea45d7c73b6cff093f453c3783ea8
Christoph Böhm <cbo@outermedia.de>
2017-11-16 19:17:59 +0100

    Implements Entry to KnowledgeCards

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './themes/sbvfrd/ts/Hydra.ts',
+    output: {
+        path: path.resolve(__dirname),
+        filename: './themes/sbvfrd/js/swissbib/swissbib.ts.js'
+        //, publicPath: '/dist'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { include: /^(?!.*_spec\.ts?$).*\.ts?$/, loader: 'ts-loader' }
+        ]
+    },
+    node: {
+        fs: "empty"
+    }
+};
