57a4a08cb9515274539d03458763b221b645079b
Christoph Böhm <cbo@outermedia.de>
2017-11-17 13:48:32 +0100

    Fixes tslint errors

--- themes/sbvfrd/ts/Hydra.ts
+++ themes/sbvfrd/ts/Hydra.ts
@@ -3,21 +3,22 @@ import {AxiosPromise, AxiosResponse, default as Axios} from "axios";
 import * as $ from "jquery";
 
 declare global {
-    let VuFind : any;
+    let VuFind: any;
 }
 
 export class Hydra {
+
+    private dataSwissbibUrl: string;
+    private axiosConfig: object;
+
     constructor(dataSwissbibUrl: string) {
         this.dataSwissbibUrl = dataSwissbibUrl;
         this.axiosConfig = {
             baseURL: dataSwissbibUrl,
-            //timeout: 100000,
-            headers: {"Accept": "application/ld+json"}
-        }
+            // timeout: 100000,
+            headers: {Accept: "application/ld+json"},
+        };
     }
-
-    private dataSwissbibUrl: string;
-    private axiosConfig: Object;
 
     /**
      * Fetches array with urls of all contributors
@@ -26,7 +27,8 @@ export class Hydra {
      * @returns {Promise<string[]>}
      */
     public getContributorUrls(bibliographicResourceId: string): Promise<string[]> {
-        return Axios.get<string[]>(this.dataSwissbibUrl + "bibliographicResource/" + bibliographicResourceId, this.axiosConfig)
+        return Axios.get<string[]>(this.dataSwissbibUrl + "bibliographicResource/" + bibliographicResourceId,
+            this.axiosConfig)
             .then((response: AxiosResponse): string[] => {
                 return response.data.contributor;
             });
@@ -37,12 +39,12 @@ export class Hydra {
      * @param {string[]} contributorUrls
      * @returns {AxiosPromise<any>[]}
      */
-    public getContributorDetails(contributorUrls: string[]): Promise<object>[] {
-        let promises: AxiosPromise<any>[] = [];
-        for (let url of contributorUrls) {
-            promises.push(Axios.get(url, this.axiosConfig).then(response => {
+    public getContributorDetails(contributorUrls: string[]): Array<Promise<object>> {
+        const promises: Array<AxiosPromise<any>> = [];
+        for (const url of contributorUrls) {
+            promises.push(Axios.get(url, this.axiosConfig).then((response) => {
                 return response.data;
-            }))
+            }));
         }
         return promises;
     }
@@ -51,10 +53,13 @@ export class Hydra {
         return Axios.get(contributorUrl, this.axiosConfig);
     }
 
-    public getContributorHtml(contributorPromise: Promise<object>): Promise<String> {
-        return contributorPromise.then(person => {
-            let p : any = person;
-            return `<li><a href="${VuFind.path}/Search/Results?lookfor=${p.lastName}, ${p.firstName}&amp;type=Author" title=" ${p.lastName}, ${p.firstName}">${p.lastName}, ${p.firstName}</a> <span ${ this.personHasSufficientData(p) ? ' class="fa fa-info-circle fa-lg"' : '' } style="display:inline;" authorid="${p["@id"]}"></span></li>`;
+    public getContributorHtml(contributorPromise: Promise<object>): Promise<string> {
+        return contributorPromise.then((person) => {
+            const p: any = person;
+            return `<li><a href="${VuFind.path}/Search/Results?lookfor=${p.lastName},
+ ${p.firstName}&amp;type=Author" title=" ${p.lastName}, ${p.firstName}">${p.lastName}, ${p.firstName}</a>
+ <span ${ this.personHasSufficientData(p) ? ' class="fa fa-info-circle fa-lg"' : "" } style="display:inline;"
+  authorid="${p["@id"]}"></span></li>`;
         });
     }
 
@@ -62,10 +67,10 @@ export class Hydra {
      * Should be more than:
      * "@context, @id, @type, id, firstName, lastName, label"
      */
-    public personHasSufficientData(data: Object) {
-        let len = Object.keys(data).length;
+    public personHasSufficientData(data: object) {
+        const len = Object.keys(data).length;
         if (len < 8) {
-            console.info(`Has only ${len} keys `, data);
+            window.console.info(`Has only ${len} keys `, data);
             return false;
         } else {
             return true;
@@ -73,28 +78,27 @@ export class Hydra {
     }
 }
 
-$(document).ready(function () {
-    console.info('Start author');
+$(document).ready(() => {
+    window.console.info("Start author");
     const client = new Hydra("http://data.swissbib.ch/");
     const recordIdEl: HTMLInputElement = $("input#record_id")[0] as HTMLInputElement;
-
 
     client.getContributorUrls(recordIdEl.value)
         .then((urls: string[]) => {
             return client.getContributorDetails(urls);
         }).then((contributors) => {
-            let list: HTMLElement = $(".sidebar .list-group")[0];
-            //$(sidebar).toggleClass("invisible");
-            //let list: JQuery<HTMLElement> = $(sidebar).append("<ul class=\"list-group\"></ul>");
+        const list: HTMLElement = $(".sidebar .list-group")[0];
+        // $(sidebar).toggleClass("invisible");
+        // let list: JQuery<HTMLElement> = $(sidebar).append("<ul class=\"list-group\"></ul>");
 
-            for (let contributor of contributors) {
-                client.getContributorHtml(contributor).then((html: string) => {
-                    console.info(html);
-                    // let el : JQuery<HTMLElement> = $(list).append(html);
-                    let el = $(html).appendTo(list);
-                    el.addClass("list-group-item");
-                });
-            }
-    })
+        for (const contributor of contributors) {
+            client.getContributorHtml(contributor).then((html: string) => {
+                window.console.info(html);
+                // let el : JQuery<HTMLElement> = $(list).append(html);
+                const el = $(html).appendTo(list);
+                el.addClass("list-group-item");
+            });
+        }
+    });
 
 });
--- themes/sbvfrd/ts/tests/Hydra_spec.ts
+++ themes/sbvfrd/ts/tests/Hydra_spec.ts
@@ -1,30 +1,29 @@
-import {Hydra} from "../Hydra";
-import Promise from "ts-promise";
 import Axios from "axios";
-
-const fs = require("fs");
+import fs from "fs";
+import Promise from "ts-promise";
+import {Hydra} from "../Hydra";
 
 const cut: Hydra = new Hydra(
-    "http://data.swissbib.ch/"
+    "http://data.swissbib.ch/",
 );
 
-function getPromiseFromFile(url : string) : Promise<Object> {
+function getPromiseFromFile(url: string): Promise<object> {
     return new Promise((resolve, reject) => {
-        let parts = url.split("/");
+        const parts = url.split("/");
         fs.readFile(`themes/sbvfrd/ts/fixtures/${ parts[parts.length - 2] }-${ parts[parts.length - 1] }.json`, "utf8",
-            (err : any, data : any) => {
+            (err: any, data: any) => {
                 if (err) {
                     reject(err);
                 }
                 resolve(
-                    {"data": JSON.parse(data)});
+                    {data: JSON.parse(data)});
             });
     });
 }
 
 beforeEach(() => {
-    Axios.get = jest.genMockFunction().mockImplementation(function (url) {
-        return getPromiseFromFile(url)
+    Axios.get = jest.genMockFunction().mockImplementation((url: string) => {
+        return getPromiseFromFile(url);
     });
 });
 
@@ -34,16 +33,16 @@ it("Should load json", () => {
     const actual: Promise<any> = cut.getContributorUrls("023426233");
     expect.assertions(2);
     // Return to evaluate promise
-    return actual.then((contributorUrls : string[]) => {
+    return actual.then((contributorUrls: string[]) => {
         expect(contributorUrls).toHaveLength(10);
         expect(contributorUrls).toContain("http://data.swissbib.ch/person/85d9afdd-b7bf-34ba-a5c7-a4c1df5c0b04");
     });
 });
 
-it("should call api with id", function() {
+it("should call api with id", () => {
     const spy = jest.spyOn(Axios, "get");
 
-    let actual = cut.getContributorUrls("023426233");
+    const actual = cut.getContributorUrls("023426233");
     expect(spy).toHaveBeenCalled();
     expect(spy).lastCalledWith(expect.stringMatching("^.*/023426233$"), expect.anything());
     spy.mockReset();
@@ -53,17 +52,16 @@ it("should call api with id", function() {
 it("should return contributor detail", () => {
     const actual = cut.getContributorDetail("http://data.swissbib.ch/person/145a6d92-7afa-3589-aba8-28e9aec9b03d");
     expect.assertions(1);
-    return actual.then(response => {
+    return actual.then((response) => {
         expect(response.data).toHaveProperty("abstract");
     });
 });
 
-
 it("should return all contributor details", () => {
-    const contributorUrls : string[] = [
+    const contributorUrls: string[] = [
         "http://data.swissbib.ch/person/145a6d92-7afa-3589-aba8-28e9aec9b03d",
         "http://data.swissbib.ch/person/ba2caead-4d66-344e-80d5-ddc6c1b87523",
-        "http://data.swissbib.ch/person/5f679432-5f41-3bd8-a19f-8a20c4431aea"/*,
+        "http://data.swissbib.ch/person/5f679432-5f41-3bd8-a19f-8a20c4431aea", /*,
         "http://data.swissbib.ch/person/d792881e-1e3e-36df-8489-a6c3ba957e24",
         "http://data.swissbib.ch/person/66f120d2-5d83-31dc-92dd-3ef48bd3a7c3",
         "http://data.swissbib.ch/person/85d9afdd-b7bf-34ba-a5c7-a4c1df5c0b04",
@@ -72,12 +70,12 @@ it("should return all contributor details", () => {
         "http://data.swissbib.ch/person/93a0ddb8-7b96-3b6d-8424-68f5ec63680d",
         "http://data.swissbib.ch/person/f2c6034e-2636-32e2-9db8-5737a263061c"*/
     ];
-    const actual : Promise<object>[] = cut.getContributorDetails(contributorUrls);
+    const actual: Array<Promise<object>> = cut.getContributorDetails(contributorUrls);
 
     expect.assertions(actual.length);
 
-    return Promise.all((actual)).then((contributors : Promise<object>) => {
-        for (let contributor of contributors) {
+    return Promise.all((actual)).then((contributors: Promise<object>) => {
+        for (const contributor of contributors) {
             expect(contributor).toHaveProperty("firstName");
         }
     });
@@ -86,26 +84,28 @@ it("should return all contributor details", () => {
 it("should create Html", () => {
     expect.assertions(1);
     window.VuFind = {
-        path: ""
+        path: "",
     };
-    const actual = cut.getContributorHtml(getPromiseFromFile( "http://data.swissbib.ch/person/5f679432-5f41-3bd8-a19f-8a20c4431aea").then((response : any) => {return response.data;}));
+    const actual = cut.getContributorHtml(
+        getPromiseFromFile("http://data.swissbib.ch/person/5f679432-5f41-3bd8-a19f-8a20c4431aea")
+            .then((response: any) => response.data));
     return expect(actual).resolves.toContain("Bamber, David");
 });
 
 it("Empty should be not sufficient info", () => {
-    let given = {};
+    const given = {};
     const actual = cut.personHasSufficientData(given);
     expect(actual).toBeFalsy();
 });
 
 it("Only 7 elements should be not sufficient info", () => {
-    let given = {"1": "", "2": "", "3": "", "4": "", "5": "", "6": "", "7": ""};
+    const given = {1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: ""};
     const actual = cut.personHasSufficientData(given);
     expect(actual).toBeFalsy();
 });
 
 it("8 elements should be sufficient info", () => {
-    let given = {"1": "", "2": "", "3": "", "4": "", "5": "", "6": "", "7": "", "8": ""};
+    const given = {1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: "", 8: ""};
     const actual = cut.personHasSufficientData(given);
     expect(actual).toBeTruthy();
 });
