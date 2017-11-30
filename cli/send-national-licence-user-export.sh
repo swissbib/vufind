329fb768945803fd475fc040ef87638f69891c4f
Lionel Walter <lionel.walter@unibas.ch>
2017-11-30 14:06:20 +0100

    fix wrong hint

--- cli/send-national-licence-user-export.sh
+++ cli/send-national-licence-user-export.sh
@@ -5,7 +5,7 @@ BASEDIR=$(dirname $0)
 INDEX="$BASEDIR/../public/index.php"
 if [ -z "$LOCAL_DIR" ] # if $LOCAL_DIR empty or unset
 then
-   echo "You need to specify the LOCAL_DIR, i.e the vufind config to use : for example : bash -c 'export LOCAL_DIR=/usr/local/vufind/httpd/local/classic/XXXXXXX; ./cli/send-national-licence-user-export.sh"
+   echo "You need to specify the LOCAL_DIR, i.e the vufind config to use : for example : bash -c 'export LOCAL_DIR=/usr/local/vufind/httpd/local/classic/XXXXXXX; ./cli/send-national-licence-user-export.sh'"
 else
    export VUFIND_LOCAL_DIR=$LOCAL_DIR;
    export VUFIND_LOCAL_MODULES=Swissbib
