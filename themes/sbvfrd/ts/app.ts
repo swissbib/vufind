409d99e0ad4cd6097364aeb94853aea18b2a52e3
Edmund Maruhn <ema@outermedia.de>
2017-12-05 11:04:07 +0100

    Integrate AutoSuggest component

--- themes/sbvfrd/ts/app.ts
+++ themes/sbvfrd/ts/app.ts
@@ -1,5 +1,6 @@
 import * as $ from "jquery";
 import {Hydra} from "./Hydra";
+import {AutoSuggest, AutoSuggestConfiguration} from "./AutoSuggest";
 
 $(document).ready(() => {
     const client = new Hydra("http://data.swissbib.ch/");
@@ -12,5 +13,17 @@ ${p.firstName}&amp;type=Author" title=" ${p.lastName}, ${p.firstName}">${p.lastN
 authorid="${p["@id"]}"></span></li>`;
     };
 
-    client.renderContributors(recordIdEl.value, list, template);
+    if (recordIdEl) {
+        client.renderContributors(recordIdEl.value, list, template);
+    }
+
+    // initialize auto-suggest
+    const autoSuggestConfiguration: AutoSuggestConfiguration = new AutoSuggestConfiguration ([
+        { label: "Abschnitt 01", limit: 3},
+        { label: "Abschnitt 02", limit: 4},
+        { label: "Abschnitt 03", limit: 5}
+    ]);
+
+    const autoSuggest = new AutoSuggest("#searchForm_lookfor", autoSuggestConfiguration);
+    autoSuggest.initialize();
 });
