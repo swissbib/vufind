302e316e44f8342dd8f2b0bfe4f2084a584980c1
Edmund Maruhn <ema@outermedia.de>
2018-01-16 00:26:09 +0100

    Correct API of person record-driver

--- module/ElasticSearch/src/ElasticSearch/View/Helper/ESPerson.php
+++ module/ElasticSearch/src/ElasticSearch/View/Helper/ESPerson.php
@@ -314,41 +314,4 @@ class ESPerson extends AbstractHelper
     {
         return $this->resolveLabelWithDisplayName('card.knowledge.person.page.link');
     }
-  
-    /**
-     * @param string      $template
-     * @param string|null $label
-     * If not null it is treated as the localization key and will be resolved before it is merged into the template.
-     * @return string
-     */
-    public function getDetailPageLink(string $template, string $label = null): string
-    {
-        $label = is_null($label)
-            ? $this->getDetailPageLinkLabel()
-            : $this->getView()->translate($label);
-
-        $segments = ['id' => $this->getPerson()->getUniqueID()];
-        $url = $this->getView()->url('page-detail-person', $segments);
-
-        return sprintf($template, $url, $label);
-    }
-
-    /**
-     * @param string $translationKeyBase
-     * @return string
-     */
-    protected function resolveLabelWithDisplayName(string $translationKeyBase)
-    {
-        $displayName = $this->getDisplayName();
-        $label = null;
-
-        if (is_null($displayName)) {
-            $label = $this->getView()->translate(sprintf('%s.no.name', $translationKeyBase));
-        } else {
-            $label = $this->getView()->translate($translationKeyBase);
-            $label = sprintf($label, $displayName);
-        }
-
-        return $label;
-    }
 }
