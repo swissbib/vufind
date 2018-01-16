44e7cccc1a586ab41a28e46331f7cf95db3d766b
Christoph Böhm <cbo@outermedia.de>
2018-01-16 12:19:53 +0100

    Applies PHP CodeSniffer

--- module/ElasticSearch/src/ElasticSearch/VuFind/RecordDriver/ElasticSearch.php
+++ module/ElasticSearch/src/ElasticSearch/VuFind/RecordDriver/ElasticSearch.php
@@ -1,12 +1,29 @@
 <?php
 /**
- * RecordDriver
+ * ElasticSearch.php
  *
- * @category ElasticSearch
- * @package  RecordDriver
- * @author   Christoph Böhm <cbo@outermedia.de>
+ * PHP Version 7
+ *
+ * Copyright (C) swissbib 2018
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA    02111-1307    USA
+ *
+ * @category VuFind
+ * @package  ElasticSearch\VuFind\RecordDriver
+ * @author   Christoph Boehm <cbo@outermedia.de>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://www.swissbib.ch/
+ * @link     http://www.vufind.org  Main Page
  */
 namespace ElasticSearch\VuFind\RecordDriver;
 
@@ -15,11 +32,20 @@ use VuFind\RecordDriver\AbstractBase;
 /**
  * Class ElasticSearch
  *
- * @package ElasticSearch\VuFind\RecordDriver
+ * @category VuFind
+ * @package  ElasticSearch\VuFind\RecordDriver
+ * @author   Christoph Boehm <cbo@outermedia.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
  */
 class ElasticSearch extends AbstractBase
 {
 
+    /**
+     * The source indentifier
+     *
+     * @var string
+     */
     protected $sourceIdentifier = 'ElasticSearch';
 
     /**
@@ -43,15 +69,23 @@ class ElasticSearch extends AbstractBase
         return $this->fields["_id"];
     }
 
+    /**
+     * Gets the Type
+     *
+     * @return mixed
+     */
     public function getType()
     {
         return $this->fields["_type"];
     }
 
     /**
-     * @param string $name
-     * @param string $prefix
-     * @param string $delimiter
+     * Gets the field
+     *
+     * @param string $name      Name of the field
+     * @param string $prefix    The prefix
+     * @param string $delimiter The delimiter
+     *
      * @return array|null
      */
     protected function getField(string $name, string $prefix, string $delimiter = ':')
@@ -59,27 +93,33 @@ class ElasticSearch extends AbstractBase
         $fieldName = $this->getQualifiedFieldName($name, $prefix, $delimiter);
 
         return array_key_exists($fieldName, $this->fields["_source"])
-          ? $this->fields["_source"][$fieldName]
-          : null;
+            ? $this->fields["_source"][$fieldName]
+            : null;
     }
 
     /**
-     * @param string $name
-     * @param string $prefix
-     * @param string $delimiter
+     * Gets the qualified field name
+     *
+     * @param string $name      The name
+     * @param string $prefix    The prefix
+     * @param string $delimiter The delimiter
+     *
      * @return string
      */
-    protected function getQualifiedFieldName(string $name, string $prefix, string $delimiter) 
+    protected function getQualifiedFieldName(string $name, string $prefix, string $delimiter)
     {
         return sprintf('%s%s%s', $prefix, $delimiter, $name);
     }
 
     /**
-     * @param $content
-     * @param string $userLocale
+     * Gets the ValueByLanguagePriority
+     *
+     * @param array  $content    The content
+     * @param string $userLocale The user locale
+     *
      * @return null
      */
-    protected function getValueByLanguagePriority($content, string $userLocale = null)
+    protected function getValueByLanguagePriority(array $content, string $userLocale = null)
     {
         $results = null;
 
@@ -108,7 +148,10 @@ class ElasticSearch extends AbstractBase
     }
 
     /**
-     * @param string $userLocale
+     * Gets the PrioritizedLocaleList
+     *
+     * @param string $userLocale The user locale
+     *
      * @return array
      */
     protected function getPrioritizedLocaleList(string $userLocale)
@@ -116,12 +159,12 @@ class ElasticSearch extends AbstractBase
         $locales = ['en', 'de', 'fr', 'it'];
         $userLocaleIndex = array_search($userLocale, $locales);
 
-        # remove user locale from its current position if available
+        // remove user locale from its current position if available
         if ($userLocaleIndex !== false) {
             array_splice($locales, $userLocaleIndex, 1);
         }
 
-        # and prepend it to gain highest priority
+        // and prepend it to gain highest priority
         array_unshift($locales, $userLocale);
 
         return $locales;
