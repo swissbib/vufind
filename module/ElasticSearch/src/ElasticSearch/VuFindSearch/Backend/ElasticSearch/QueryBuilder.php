61f4ef17399a6dfb2f670d066ea233daffd604bd
Christoph Böhm <cbo@outermedia.de>
2018-01-16 12:01:20 +0100

    Applies PHP CodeSniffer

--- module/ElasticSearch/src/ElasticSearch/VuFindSearch/Backend/ElasticSearch/QueryBuilder.php
+++ module/ElasticSearch/src/ElasticSearch/VuFindSearch/Backend/ElasticSearch/QueryBuilder.php
@@ -1,9 +1,29 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: boehm
- * Date: 23.11.17
- * Time: 13:41
+ * QueryBuilder.php
+ *
+ * PHP Version 7
+ *
+ * Copyright (C) swissbib 2018
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA    02111-1307    USA
+ *
+ * @category VuFind
+ * @package  ElasticSearch\VuFindSearch\Backend\ElasticSearch
+ * @author   Christoph Boehm <cbo@outermedia.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
  */
 namespace ElasticSearch\VuFindSearch\Backend\ElasticSearch;
 
@@ -11,20 +31,33 @@ use VuFindSearch\Backend\Solr\QueryBuilderInterface;
 use VuFindSearch\ParamBag;
 use VuFindSearch\Query\AbstractQuery;
 
+/**
+ * Class QueryBuilder
+ *
+ * @category VuFind
+ * @package  ElasticSearch\VuFindSearch\Backend\ElasticSearch
+ * @author   Christoph Boehm <cbo@outermedia.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
+ */
 class QueryBuilder implements QueryBuilderInterface
 {
     /**
+     * The templates
+     *
      * @var array
      */
-    private $templates;
+    private $_templates;
 
     /**
      * QueryBuilder constructor.
+     *
+     * @param array $templates The templates
      */
     public function __construct(array $templates)
     {
 
-        $this->templates = $templates;
+        $this->_templates = $templates;
     }
 
     /**
@@ -37,7 +70,7 @@ class QueryBuilder implements QueryBuilderInterface
     public function build(AbstractQuery $query)
     {
         $paramBag = new ParamBag();
-        $paramBag->set("templates", $this->templates);
+        $paramBag->set("templates", $this->_templates);
         // TODO Very basic approach
         $queryString = $query->getString();
         $paramBag->set('q', $queryString);
