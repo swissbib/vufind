09811e702b3e6b6d51f7fbe3223458b020632fe5
Christoph Böhm <cbo@outermedia.de>
2018-01-16 12:22:22 +0100

    Applies PHP CodeSniffer

--- module/ElasticSearch/src/ElasticSearch/VuFind/RecordDriver/Factory.php
+++ module/ElasticSearch/src/ElasticSearch/VuFind/RecordDriver/Factory.php
@@ -1,12 +1,29 @@
 <?php
 /**
- * Factory for RecordDriver
+ * Factory.php
  *
- * @category ElasticSearch
- * @package  RecordDriver
- * @author   Christoph Böhm <cbo@outermedia.de>
+ * PHP Version 7
+ *
+ * Copyright (C) swissbib 2018
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA    02111-1307    USA
+ *
+ * @category VuFind
+ * @package  ElasticSearch\VuFind\RecordDriver
+ * @author   Christoph Boehm <cbo@outermedia.de>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://www.swissbib.ch/
+ * @link     http://www.vufind.org  Main Page
  */
 namespace ElasticSearch\VuFind\RecordDriver;
 
@@ -15,10 +32,21 @@ use Zend\ServiceManager\ServiceManager;
 /**
  * Class Factory
  *
- * @package ElasticSearch\VuFind\RecordDriver
+ * @category VuFind
+ * @package  ElasticSearch\VuFind\RecordDriver
+ * @author   Christoph Boehm <cbo@outermedia.de>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
  */
 class Factory
 {
+    /**
+     * Creates an ElasticSearchRecord
+     *
+     * @param \Zend\ServiceManager\ServiceManager $sm The Service Manager
+     *
+     * @return \ElasticSearch\VuFind\RecordDriver\ElasticSearch
+     */
     public static function getElasticSearchRecord(ServiceManager $sm)
     {
         $driver = new ElasticSearch(
@@ -30,6 +58,13 @@ class Factory
         return $driver;
     }
 
+    /**
+     * Creates a PersonRecord
+     *
+     * @param \Zend\ServiceManager\ServiceManager $sm The Service Manager
+     *
+     * @return \ElasticSearch\VuFind\RecordDriver\ESPerson
+     */
     public static function getESPersonRecord(ServiceManager $sm)
     {
         $driver = new ESPerson(
@@ -41,11 +76,25 @@ class Factory
         return $driver;
     }
 
+    /**
+     * Creates a BibliographicResourceRecord
+     *
+     * @param \Zend\ServiceManager\ServiceManager $sm The Service Manager
+     *
+     * @return \ElasticSearch\VuFind\RecordDriver\ESBibliographicResource
+     */
     public static function getESBibliographicResourceRecord(ServiceManager $sm)
     {
         return new ESBibliographicResource();
     }
 
+    /**
+     * Creates a SubjectRecord
+     *
+     * @param \Zend\ServiceManager\ServiceManager $sm The Service Manager
+     *
+     * @return \ElasticSearch\VuFind\RecordDriver\ESSubject
+     */
     public static function getESSubjectRecord(ServiceManager $sm)
     {
         return new ESSubject();
