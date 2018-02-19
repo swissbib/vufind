531ea1a08275975db5f7aec567dcd95c90dd6412
Edmund Maruhn <ema@outermedia.de>
2018-02-19 09:21:48 +0100

    Extend client API for carousels to handle newly introduced displayName field

--- themes/sbvfrd/ts/carousel/DataEntry.ts
+++ themes/sbvfrd/ts/carousel/DataEntry.ts
@@ -14,9 +14,14 @@ export default interface DataEntry {
     readonly thumbnail: string;
 
     /**
-     * The name of the person, subject or media.
+     * The name of the person, subject or media to use for further searches.
      */
     readonly name: string;
+
+    /**
+     * Name of the person, subject or media which to use for rendering.
+     */
+    readonly displayName: string;
 
     /**
      * Indicates whether there is sufficient data for this entry to show in a knowledge-card.
