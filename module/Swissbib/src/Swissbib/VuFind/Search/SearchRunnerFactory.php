c7c8c09c08b0e38f423c9e991401b36c91fa5c8b
guenter hipler <guenter.hipler@unibas.ch>
2018-05-31 10:38:02 +0200

    sorry - forgot to add the latest changes in our own SearchRunnerFactory

--- module/Swissbib/src/Swissbib/VuFind/Search/SearchRunnerFactory.php
+++ module/Swissbib/src/Swissbib/VuFind/Search/SearchRunnerFactory.php
@@ -1,4 +1,44 @@
 <?php
+
+/**
+ * Search runner factory.
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2018.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Search
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
+namespace Swissbib\VuFind\Search;
+
+use Interop\Container\ContainerInterface;
+use Interop\Container\Exception\ContainerException;
+use Zend\EventManager\EventManager;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
+use Zend\ServiceManager\Factory\FactoryInterface;
+use VuFind\Search\SearchRunner as VFSearchRunner;
+
+
+
+
 /**
  * SearchRunnerFactory
  *
@@ -39,7 +79,54 @@
  * @link     http://vufind.org
  * @link     http://www.swissbib.ch
  */
-class SearchRunnerFactory
+class SearchRunnerFactory implements FactoryInterface
 {
 
+    protected $extendedTargets;
+
+
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+
+
+        //if (!isset($this->extendedTargets)) {
+        //    $mainConfig = $container->get('VuFind\Config\PluginManager')
+        //        ->get('config');
+        //    $extendedTargetsSearchClassList
+        //        = $mainConfig->SwissbibSearchExtensions->extendedTargets;
+
+        //    $this->extendedTargets = array_map(
+        //        'trim', explode(',', $extendedTargetsSearchClassList)
+        //    );
+        //}
+
+        //if (in_array($this->searchClassId, $this->extendedTargets)) {
+        //    return new SearchRunner($container->get('VuFind\SearchResultsPluginManager'),
+        //        new EventManager($container->get('SharedEventManager')));
+
+                //$this->serviceLocator
+                //->get('VuFind\SearchResultsPluginManager');
+        //}
+
+
+
+
+        //@Matthias: hier wie besprochen einen "whitelist" Mechanismus über module-config?
+
+        //return new VFSearchRunner($container->get('VuFind\Search\Results\PluginManager'),
+        //    new EventManager($container->get('SharedEventManager')));
+
+        //@matthias: hat VuFind jetzt unterschiedliche SearchRunner, so dass sie die Instanz
+        //über $requestedName erstellen??
+
+        //@Matthias: Die Factory von VuFind benutzt jetzt diesen namespace
+        //VuFind\Search\Results\PluginManager
+        // - können wir das auch machen?
+        // - brauchen wir unseren alten?
+        //ich überblicke das im Moment noch nicht alles...
+        return new $requestedName($container->get('VuFind\Search\Results\PluginManager'),
+            new EventManager($container->get('SharedEventManager')));
+
+
+    }
 }
