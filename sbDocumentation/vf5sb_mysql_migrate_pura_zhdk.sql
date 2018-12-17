cd27d15c761f712b182050a99c0848c6818e546b
Lionel Walter <lionel.walter@unibas.ch>
2018-12-17 11:34:23 +0100

    use pura-back-end-test user on test

--- sbDocumentation/vf5sb_mysql_migrate_pura_zhdk.sql
+++ sbDocumentation/vf5sb_mysql_migrate_pura_zhdk.sql
@@ -3,3 +3,6 @@
 --
 ALTER TABLE `pura_user`
   ADD COLUMN `is_member_education_institution` BOOLEAN NOT NULL DEFAULT FALSE AFTER `last_account_extension_request` ;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON pura_user to 'pura-back-end-test'@'%';
+GRANT SELECT ON user to 'pura-back-end-test'@'%';
