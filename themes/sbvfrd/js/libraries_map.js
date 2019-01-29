d55b7bd64668e82e1431bd0c19c3f7df6f3bdfb8
Lionel Walter <lionel.walter@unibas.ch>
2019-01-29 17:44:27 +0100

    translated labels on the map

--- themes/sbvfrd/js/libraries_map.js
+++ themes/sbvfrd/js/libraries_map.js
@@ -9,11 +9,20 @@ $.getJSON("/themes/sbvfrd/js/raw_map_fr.json", function(libraries){
     id: 'mapbox.streets'
   }).addTo(mymap);
 
-  var markers = L.markerClusterGroup();
+  var markers = L.markerClusterGroup({
+    iconCreateFunction: function(cluster) {
+      return L.divIcon({ html: '<span class="btn btn-primary"><b>' + cluster.getChildCount() + '</b>&nbsp;&nbsp;<i class="fa fa-institution"></i></span>' });
+    }
+  });
 
   var geoJsonLayer = L.geoJson(libraries, {
     onEachFeature: function (feature, layer) {
-      layer.bindPopup(feature.properties.label.de);
+      layer.bindPopup(
+        '<b>'+feature.properties.label.de+'</b><br>' +
+        feature.properties.group_label.de+'<br>' +
+        '<a href="'+feature.properties.url.de+'">' + Library_Information + '</a><br>' +
+        '<a href="/Search/Results?lookfor=&type=AllFields&filter%5B%5D=institution%3A%22'+feature.properties.bib_code+'%22">' + Search_This_Library +'</a>'
+      );
     }
   });
 
