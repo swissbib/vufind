4eeb89f6a6891fdab167cfd08d6dff7ffbf1e563
edelm <matthias.edel@unibas.ch>
2019-05-13 16:34:57 +0200

    fixing npm run tests

--- themes/sbvfrd/ts/RecordRenderer.ts
+++ themes/sbvfrd/ts/RecordRenderer.ts
@@ -34,7 +34,7 @@ export default class RecordRenderer {
                         const elements: HTMLElement[] = [];
                         for (const detail of details) {
                             elements.push(
-                                this.renderDetails(detail, template, htmlList),
+                                this.renderDetails(detail.data, template, htmlList),
                             );
                         }
                         return elements;
