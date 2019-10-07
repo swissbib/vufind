5672ee49876f2d1c12c354d02098316f69013cc3
edelmatthias <matthias.edel@unibas.ch>
2019-10-07 10:51:23 +0200

    codestyle fix

--- module/Swissbib/src/Swissbib/RecordDriver/Helper/Holdings.php
+++ module/Swissbib/src/Swissbib/RecordDriver/Helper/Holdings.php
@@ -609,7 +609,7 @@ class Holdings
             $idls = $firstItem['bib_library'];
             if ($idls == '' && $firstItem['network'] == 'RERO') {
                 $idls = 'RERO';
-            } else if ($idls == '' && $firstItem['network'] == 'SNL') {
+            } elseif ($idls == '' && $firstItem['network'] == 'SNL') {
                 $idls = 'SNL';
             }
 
