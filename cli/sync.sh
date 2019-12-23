42957f08790f388c416bd985ba8349f4c74e10cf
edelmatthias <matthias.edel@unibas.ch>
2019-12-23 17:53:30 +0100

    set databases connections to new dbs

--- cli/sync.sh
+++ cli/sync.sh
@@ -17,11 +17,11 @@ fi
 export VUFIND_CACHE=$VUFIND_LOCAL_DIR/cache
 export VUFIND_LOCAL_MODULES=Swissbib
 
-su -c "php $INDEX libadmin sync $@" swissbib
+su -c "php $INDEX libadmin sync $@" matthias
 
-su -c "php $INDEX libadmin syncGeoJson $@" swissbib
+su -c "php $INDEX libadmin syncGeoJson $@" matthias
 #symbolic link so that geojson.json is reachable for libraries_map.js
-su -c "ln -s ../data/cache/geojson.json $BASEDIR/../public/geojson.json" swissbib
+su -c "ln -s ../data/cache/geojson.json $BASEDIR/../public/geojson.json" matthias
 
 #please do not delete a directory with options -rf as root based on a relative directory! GH
 echo "Trying to remove local cache"
