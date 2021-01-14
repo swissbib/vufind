79e47dd7cea37b37d30f823c99e5abe1c83f6ab6
Siegfried Ballmann <sba@outermedia.de>
2021-01-14 15:37:54 +0100

    SC-90: Improves code style.

--- module/SwissCollections/src/SwissCollections/RecordDriver/FieldRenderContext.php
+++ module/SwissCollections/src/SwissCollections/RecordDriver/FieldRenderContext.php
@@ -1,4 +1,32 @@
 <?php
+/**
+ * SwissCollections: FieldRenderContext.php
+ *
+ * PHP version 7
+ *
+ * Copyright (C) project swissbib, University Library Basel, Switzerland
+ * http://www.swisscollections.org  / http://www.swisscollections.ch / http://www.ub.unibas.ch
+ *
+ * Date: 1/12/20
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category SwissCollections_VuFind
+ * @package  SwissCollections\RecordDriver
+ * @author   Lionel Walter <lionel.walter@unibas.ch>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.swisscollections.org Project Wiki
+ */
 
 namespace SwissCollections\RecordDriver;
 
@@ -6,6 +34,15 @@ use SwissCollections\Formatter\FieldFormatterData;
 use SwissCollections\Formatter\FieldFormatterRegistry;
 use SwissCollections\Formatter\SubfieldFormatterRegistry;
 
+/**
+ * Context for field formatters.
+ *
+ * @category SwissCollections_VuFind
+ * @package  SwissCollections\RecordDriver
+ * @author   Lionel Walter <lionel.walter@unibas.ch>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development Wiki
+ */
 class FieldRenderContext
 {
 
@@ -13,16 +50,22 @@ class FieldRenderContext
     public $processedSubMaps;
 
     /**
+     * The registry of all field formatters.
+     *
      * @var FieldFormatterRegistry
      */
     protected $fieldFormatterRegistry;
 
     /**
+     * The registry of all subfield formatters.
+     *
      * @var SubfieldFormatterRegistry
      */
     protected $subfieldFormatterRegistry;
 
     /**
+     * The marc record.
+     *
      * @var SolrMarc
      */
     public $solrMarc;
@@ -30,11 +73,13 @@ class FieldRenderContext
     /**
      * FieldRenderContext constructor.
      *
-     * @param FieldFormatterRegistry $fieldFormatterRegistry
-     * @param SolrMarc               $solrMarc
+     * @param FieldFormatterRegistry    $fieldFormatterRegistry    the field formatter registry
+     * @param SolrMarc                  $solrMarc                  the marc record
+     * @param SubfieldFormatterRegistry $subfieldFormatterRegistry the subfield formatter registry
      */
-    public function __construct($fieldFormatterRegistry, SolrMarc $solrMarc, $subfieldFormatterRegistry)
-    {
+    public function __construct(
+        $fieldFormatterRegistry, $solrMarc, $subfieldFormatterRegistry
+    ) {
         $this->solrMarc = $solrMarc;
         $this->fieldFormatterRegistry = $fieldFormatterRegistry;
         $this->subfieldFormatterRegistry = $subfieldFormatterRegistry;
@@ -42,42 +87,66 @@ class FieldRenderContext
     }
 
     /**
-     * @param  String $candidate
+     * Checks, whether a value was already rendered.
+     *
+     * @param string $candidate a lookup key build from values to render
+     *
      * @return bool
      */
-    public function alreadyProcessed(String $candidate): bool
+    public function alreadyProcessed(string $candidate): bool
     {
         return $this->processedSubMaps[$candidate] === true;
     }
 
-    public function addProcessed(String $candidate)
+    /**
+     * Remember an already rendered value.
+     *
+     * @param string $candidate the value's lookup key
+     *
+     * @return void
+     */
+    public function addProcessed(string $candidate)
     {
         $this->processedSubMaps[$candidate] = true;
     }
 
     /**
-     * @param String               $lookupKey
-     * @param FieldFormatterData[] $data
-     * @param String               $renderMode
-     * @param String               $labelKey
-     * @param FieldRenderContext   $context
+     * Apply a field formatter.
+     *
+     * @param string               $lookupKey  a value's lookup key
+     * @param FieldFormatterData[] $data       the values to render
+     * @param string               $renderMode the field formatter's name
+     * @param string               $labelKey   the field's translation key
+     * @param FieldRenderContext   $context    the render context
+     *
+     * @return void
      */
-    public function applyFieldFormatter($lookupKey, &$data, $renderMode, $labelKey, $context): void
-    {
-        $this->fieldFormatterRegistry->applyFormatter($renderMode, $labelKey, $data, $context);
+    public function applyFieldFormatter(
+        $lookupKey, &$data, $renderMode, $labelKey, $context
+    ): void {
+        $this->fieldFormatterRegistry->applyFormatter(
+            $renderMode, $labelKey, $data, $context
+        );
         $this->addProcessed($lookupKey);
     }
 
     /**
-     * @param String             $lookupKey
-     * @param FieldFormatterData $data
-     * @param String             $renderMode
-     * @param String             $labelKey
-     * @param FieldRenderContext $context
+     * Apply a subfield formatter.
+     *
+     * @param String             $lookupKey  a value's lookup key
+     * @param FieldFormatterData $data       the value to render
+     * @param String             $renderMode the field formatter's name
+     * @param String             $labelKey   the field's translation key
+     * @param FieldRenderContext $context    the render context
+     *
+     * @return void
      */
-    public function applySubfieldFormatter($lookupKey, &$data, $renderMode, $labelKey, $context)
-    {
-        $this->subfieldFormatterRegistry->applyFormatter($renderMode, $labelKey, $data, $context);
+    public function applySubfieldFormatter(
+        $lookupKey, &$data, $renderMode, $labelKey, $context
+    ) {
+        $this->subfieldFormatterRegistry->applyFormatter(
+            $renderMode, $labelKey, $data, $context
+        );
         if (!empty($lookupKey)) {
             $this->addProcessed($lookupKey);
         }
